fd00ae2069218fb685cac87020f118efa6bf152a
joao-bettu <joaobettuvictor@gmail.com>
2025-05-05 11:10:04 -0300

    Get Pokemon Info function

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -9,13 +9,45 @@ radios.forEach((radio) => {
         const value = radio.value;
         const sections = document.getElementsByClassName("hidden");
 
-        function showAndHide (index, section) {
+        const randomId = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+        const showAndHide = (index, section) => {
             for (let i = 0; i < section.length; i++) {
                 if (i === index) {
                     section[i].style.display = "block";
                 } else {
                     section[i].style.display = "none";
                 }
+            }
+        }
+        const getPokeInfo = (id, name) => {
+            if (typeof id === "number") {
+                if (id >= 1 && id <= 1025) {
+                    fetch(`${ApiUrl}pokemon/${id}`)
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data);
+                        })
+                        .catch(error => console.log(error));
+                } else {
+                    console.log("Invalid ID!");
+                }
+            } else {
+                console.log("ID is not a number!");
+            }
+
+            if (typeof name === "string") {
+                if (name != "") {
+                    fetch(`${ApiUrl}pokemon/${name}`)
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data);
+                        })
+                        .catch(error => console.log(error));
+                } else {
+                    console.log("Invalid Name!");
+                }
+            } else {
+            console.log("Name is not a string!");
             }
         }
 
@@ -25,6 +57,10 @@ radios.forEach((radio) => {
                 break;
             case "poke-name":
                 showAndHide(1, sections);
+                break;
+            case "radom-pokemon":
+                const randomPokemon = randomId(1, 1025);
+                console.log(randomPokemon);
                 break;
             default:
                 console.log(value);
